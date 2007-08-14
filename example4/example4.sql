f31bfdf7e9a5ee8beb21952df73e51f89bade9cb
pokorra <pokorra>
2007-08-14 22:31:53 +0000

    example mysql

---
+++ example4/example4.sql
@@ -0,0 +1,10 @@
+-- mysql
+CREATE TABLE stats (
+ name varchar(64) NOT NULL default '',
+ value int(11) unsigned NOT NULL default '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE stats2 (
+ name varchar(64) NOT NULL default '',
+ value int(11) unsigned NOT NULL default '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
